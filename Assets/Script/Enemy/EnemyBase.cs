b1dab806d8e948cff9b389638e78df8214057979
Enzeun <khuynjun123@naver.com>
2026-07-29 15:56:03 +0900

    [feat] 적 클래스 메서드 구현
    이동
    데미지 입기
    죽기
    애니메이션
    구현

--- Assets/Script/Enemy/EnemyBase.cs
+++ Assets/Script/Enemy/EnemyBase.cs
@@ -1,23 +1,238 @@
 using DG.Tweening;
 using Sirenix.OdinInspector;
+using System;
 using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+    // 컴포넌트 참조
     [BoxGroup("Required Component"), SerializeField, Required]
     private DOTweenAnimation myAnim;
+    [BoxGroup("Required Component"), SerializeField, Required]
+    private Rigidbody2D myRigidbody;
+    [BoxGroup("Required Component"), SerializeField, Required]
+    private SpriteRenderer mySpriteRenderer;
+    [BoxGroup("Required Component"), SerializeField, Required]
+    private Animator myAnimator;
+
+    // 애니메이션 해시
+    private static int MovingHash = Animator.StringToHash("IsMoving");
+    private static int DieHash = Animator.StringToHash("IsDead");
+
+    // 몬스터 스탯 필드
+    [BoxGroup("Enemy Status"), ShowInInspector]
+    public float maxHp { get; private set; } = 100f;
+    [BoxGroup("Enemy Status"), ShowInInspector]
+    public float hp { get; private set; } = 100f;
+    [BoxGroup("Enemy Status"), ShowInInspector]
+    public float movementSpeed = 1f;
+    [BoxGroup("Enemy Status"), ShowInInspector]
+    public bool isDead { get; private set; } = false;
+    [BoxGroup("Enemy Status"), ShowInInspector]
+    public bool canMove { get; private set; } = true;
+    [BoxGroup("Enemy Status"), SerializeField]
+    private PlayerBase _player;
+    [BoxGroup("Enemy Status"), SerializeField]
+    private float attackRange = 1f;
+    [BoxGroup("Enemy Status"), SerializeField]
+    private float attackPower = 10f;
+    [BoxGroup("Enemy Status"), SerializeField]
+    private float attackDelay = 1f;
+    [BoxGroup("Enemy Status"), SerializeField]
+    private float attackTimer;
 
 
+    // 현재 상태들
+    [BoxGroup("Current Status"), ReadOnly, ShowInInspector]
+    private Vector2 movingDirection;
+    [BoxGroup("Current Status"), ReadOnly, ShowInInspector]
+    private bool isLookRight = true;
+    [BoxGroup("Current Status"), ReadOnly, ShowInInspector]
+    private Vector2 distance;
+    [BoxGroup("Current Status"), ReadOnly, ShowInInspector]
+    private bool isInRange;
+    [BoxGroup("Current Status"), ReadOnly, ShowInInspector]
+    private bool isInAttackRange;
+    [BoxGroup("Current Status"), ReadOnly, ShowInInspector]
+    private bool isMoving;
 
-    [ButtonGroup("메서드 디버깅")]
+    public void Initialize(PlayerBase player)
+    {
+        _player = player;
+        canMove = true;
+        isDead = false;
+        isLookRight = true;
+        isInRange = false;
+        isInAttackRange = false;
+        attackTimer = attackDelay;
+        hp = maxHp;
+        myAnimator.SetBool(DieHash, false);
+
+    }
+
+    private void Update()
+    {
+        CalculateDistance();
+        CalculateMovingDirection();
+        TryAttackPlayer();
+    }
+    private void FixedUpdate()
+    {
+        MoveToPlayer();
+    }
+
+
+    [BoxGroup("메서드 디버깅"), Button]
     public void TakeDamage(float damage)
     {
+        if (isDead) return;
+
+        hp = Mathf.Clamp(hp - damage, 0, maxHp);
+
+        if (hp <= 0)
+        {
+            Die();
+        }
+
         PlayHurtAnim();
     }
 
-    [ButtonGroup("메서드 디버깅")]
+    [BoxGroup("메서드 디버깅"), Button]
     public void PlayHurtAnim()
     {
         myAnim.DORestart();
     }
+
+    [BoxGroup("메서드 디버깅"), Button]
+    private void Die()
+    {
+        canMove = false;
+
+        isDead = true;
+
+        PlayDieAnimation();
+
+        OnDie?.Invoke();
+    }
+
+    private void PlayDieAnimation()
+    {
+        myAnimator.SetBool(DieHash, true);
+    }
+
+    public Action OnDie;
+
+    //-------------------------------------------------------------------------------------
+
+    // 이동 처리. 몬스터의 이동은 단순하므로 클래스를 따로 나누지 않음
+
+    private void CalculateDistance()
+    {
+        if (isDead) return;
+
+        if (!canMove) return;
+
+        distance = (_player.transform.position - gameObject.transform.position);
+
+        isInAttackRange = (distance.magnitude <= attackRange);
+
+        isInRange = (distance.magnitude <= (attackRange * 0.8f));
+    }
+
+
+    private void CalculateMovingDirection()
+    {
+        if (isDead) return;
+
+        if (!canMove) return;
+
+        movingDirection = distance.normalized;
+
+        FlipEnemySprite();
+    }
+
+    private void FlipEnemySprite()
+    {
+        // 한 번 거르기 때문에 죽었는지 확인 X
+        bool currentLookRight = movingDirection.x >= 0;
+
+        if (isLookRight == currentLookRight) return;
+
+        isLookRight = currentLookRight;
+
+        mySpriteRenderer.flipX = !isLookRight;
+    }
+
+    private void MoveToPlayer()
+    {
+        if (isDead || !canMove || isInRange)
+        {
+            if (isMoving)
+            {
+                isMoving = false;
+                SetMovingAnimation();
+            }
+            return;
+        }
+
+        myRigidbody.linearVelocity = movingDirection * movementSpeed;
+
+        if (!isMoving)
+        {
+            isMoving = true;
+
+            SetMovingAnimation();
+        }
+    }
+
+    private void SetMovingAnimation()
+    {
+        myAnimator.SetBool(MovingHash, isMoving);
+    }
+
+    //--------------------------------------------------------------------------
+
+    private void TryAttackPlayer()
+    {
+        if (isDead) return;
+
+        if (isInAttackRange)
+        {
+            attackTimer -= Time.deltaTime;
+
+            if (attackTimer <= 0)
+            {
+                AttackPlayer(attackPower);
+                attackTimer = attackDelay;
+                return;
+            }
+        }
+
+        if (attackTimer != attackDelay)
+        {
+            attackTimer = attackDelay;
+            return;
+        }
+    }
+
+    [BoxGroup("메서드 디버깅"), Button]
+
+    private void AttackPlayer(float damage)
+    {
+        _player.TakeDamage(damage);
+    }
+
+
+
+    //--------------------------------------------------------------------------
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, attackRange);
+        Gizmos.color = Color.coral;
+        Gizmos.DrawWireSphere(gameObject.transform.position, (attackRange * 0.8f));
+    }
+
+
 }
--- Assets/Script/Player/PlayerBase.cs
+++ Assets/Script/Player/PlayerBase.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using Sirenix.OdinInspector;
+using System;
 using UnityEngine;
 
 public class PlayerBase : MonoBehaviour
@@ -12,27 +13,51 @@ public class PlayerBase : MonoBehaviour
     [BoxGroup("Player Status"), ShowInInspector]
     public bool isDead { get; private set; } = false;
     [BoxGroup("Player Status"), ShowInInspector]
-    public bool canMove { get; private set; } = false;
+    public bool canMove { get; private set; } = true;
 
     [BoxGroup("Require!!"), SerializeField, Required]
     private DOTweenAnimation myDOTween;
 
 
-
     // 데미지 입는 곳
-    [ButtonGroup("메서드 디버깅")]
+    [BoxGroup("메서드 디버깅"), Button]
     public void TakeDamage(float damage)
     {
         if (isDead) return;
 
+        float prevHp = hp;
+
         hp = Mathf.Clamp(hp - damage, 0, maxHp);
+
+        OnDamaged?.Invoke(prevHp, hp);
+
+        if (hp <= 0)
+        {
+            Die();
+        }
 
         PlayHurtAnimation();
     }
 
-    [ButtonGroup("메서드 디버깅")]
+    [BoxGroup("메서드 디버깅"), Button]
     private void PlayHurtAnimation()
     {
         myDOTween.DORestartAllById("Hurt");
     }
+
+    [BoxGroup("메서드 디버깅"), Button]
+    private void Die()
+    {
+        canMove = false;
+
+        isDead = true;
+
+        OnDie?.Invoke();
+    }
+
+    public Action OnDie;
+    /// <summary>
+    /// 반환 : 이전체력, 현재체력
+    /// </summary>
+    public Action<float, float> OnDamaged;
 }
